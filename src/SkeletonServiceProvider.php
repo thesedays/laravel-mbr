4a03d53f1365269d4f27bc96b715c9d2f3d542bc
Joren Van Hocht <jorenvh@gmail.com>
2016-11-03 16:52:55 +0100

    Add config file

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -9,26 +9,16 @@ class SkeletonServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-           $this->publishes([
-               __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
-           ], 'config');
-
-           /*
-           $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
-   
-           $this->publishes([
-               __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
-           ], 'views');
-           */
-        }
-    }
+       $this->publishes([
+           __DIR__.'/../config/mbr.php' => config_path('mbr.php'),
+       ], 'config');
+     }
 
     /**
      * Register the application services.
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'skeleton');
+        $this->mergeConfigFrom(__DIR__.'/../config/mbr.php', 'mbr');
     }
 }
